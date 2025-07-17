c6939e407e8b919864e507607066a2328d87b4bb
FrancescoSls97 <frankds1997@gmail.com>
2025-07-17 16:39:03 +0200

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,21 @@
+export default function Main() {
+    return (
+        <main>
+            <div className="container m-5 p-5">
+                <div className="card">
+                    <div className="card-header">
+                        <h1>Learn Web development</h1>
+                    </div>
+                    <div className="card-body">
+                        <button className="btn btn-primary m-3 p-3">HTML</button>
+                        <button className="btn btn-primary m-3 p-3">CSS</button>
+                        <button className="btn btn-primary m-3 p-3">Java Script</button>
+                        <button className="btn btn-primary m-3 p-3">Node.js</button>
+                        <button className="btn btn-primary m-3 p-3">Express</button>
+                        <button className="btn btn-primary m-3 p-3">React</button>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
