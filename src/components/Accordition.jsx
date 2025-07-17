1af0249161644040f87008254ba8f68eb9673ebe
FrancescoSls97 <frankds1997@gmail.com>
2025-07-17 19:29:24 +0200

    accordition list in card

--- src/components/Accordition.jsx
+++ src/components/Accordition.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 export default function AccordionList (){
 const[open, setOpen] = useState(false)
 const data = [
-    {
+    {   
         language: 'HTML',
         info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, ullam. Sit, dolores eum animi saepe id voluptate recusandae error sapiente. Facilis, pariatur impedit officiis sed porro dignissimos! Temporibus, aperiam. Distinctio.'
     },
@@ -28,21 +28,23 @@ const data = [
     }
 ]
 
-function handleClick(){
+function handleClick(index){
     console.log('click');
+    console.log(index);
     
-    setOpen(!open)
+    setOpen(index)
 }
     return(
-        <div id="accordition" className="bg-black text-white">
-            {data.map((item) =>{
+        <div id="accordition" className="bg-white text-dark">
+            {data.map((item, index)=> {
                 return(
                     <div className="accordion-item">
-                        <h3><button onClick={handleClick}>{item.language}</button></h3>
-                        { open && <p>{item.info}</p>}
+                        <button onClick={()=> handleClick(index)}>{item.language}</button>
+                        { open === index && <p>{item.info}</p>}
                     </div>
                 )
             })}
         </div>
     )
 }
+
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import AccordionList from "./Accordition"
 
 export default function Main() {
 
-    return (
+return (
         <main>
             <div className="container m-5 p-5">
                 <div className="card">
@@ -11,17 +11,11 @@ export default function Main() {
                         <h1>Learn Web development</h1>
                     </div>
                     <div className="card-body">
-                        <button className="btn btn-primary m-3 p-3">HTML</button>
-                        <button className="btn btn-primary m-3 p-3">CSS</button>
-                        <button className="btn btn-primary m-3 p-3">Java Script</button>
-                        <button className="btn btn-primary m-3 p-3">Node.js</button>
-                        <button className="btn btn-primary m-3 p-3">Express</button>
-                        <button className="btn btn-primary m-3 p-3">React</button>
+
+                        <AccordionList />                        
+
                     </div>
                 </div>
-            </div>
-            <div className="container">
-                <AccordionList></AccordionList>
             </div>
         </main>
     )
